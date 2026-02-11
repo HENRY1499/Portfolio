f5148e55245d9809ba0c07f5816587876969087e
Henry Castillo <hdaniel031499@gmail.com>
2026-02-11 11:07:21 -0500

    Toggle Listo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import path from "path";
 // https://astro.build/config
 export default defineConfig({
-  
   vite: {
+    resolve: {
+      alias: {
+        "@layouts": path.resolve("./src/layouts"),
+      },
+    },
     plugins: [tailwindcss()],
   },
   integrations: [
