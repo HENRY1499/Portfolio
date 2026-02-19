975449da20904d7f2c2ee53ff9cb7468afd27789
Henry Castillo <hdaniel031499@gmail.com>
2026-02-19 09:39:41 -0500

    Navbar Responsive - 100%

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,10 +1,12 @@
 import type { APIRoute } from "astro";
+
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const name = data.get("name")?.toString().trim();
   const email = data.get("email")?.toString().trim();
   const message = data.get("message")?.toString().trim();
   const whatsapp = data.get("whatsapp")?.toString().trim();
+  
   if (name === "" || email === "" || message === "" || whatsapp === "") {
     return new Response("Faltan campos por completar", { status: 400 });
   }
