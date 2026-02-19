7f4f60e3426235969e34a1914ad0e189fb17804e
Henry Castillo <hdaniel031499@gmail.com>
2026-02-19 16:37:41 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  server: {
+    host: true, // 👈 ESTO ES LA CLAVE
+  },
   vite: {
     resolve: {
       alias: {
