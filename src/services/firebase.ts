0855c03ecdc79a894dbd695cae9dc4def2d11ff3
Henry Castillo <hdaniel031499@gmail.com>
2026-02-21 09:48:42 -0500

    .

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,14 +1,8 @@
 import admin, { type ServiceAccount } from "firebase-admin";
-import fs from "fs";
 
 const serviceAccount = JSON.parse(
-  fs.readFileSync("/etc/secrets/serviceAccount.json", "utf8"),
+  Buffer.from(import.meta.env.FIREBASE_ADMIN_API!, "base64").toString("utf8"),
 );
-// import serviceAccount from "../../serviceAccount.json";
-
-// const serviceAccount = JSON.parse(
-//   Buffer.from(import.meta.env.FIREBASE_ADMIN_API!, "base64").toString("utf8"),
-// );
 
 if (!admin.apps.length) {
   admin.initializeApp({
